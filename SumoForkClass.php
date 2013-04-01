195e0c4142966a8c312be96a139ff494bc6373c4
Tijs Verkoyen <tijs@verkoyen.eu>
2013-04-01 11:57:18 +0200

    Added method that initializes our Errbit-errorhandler

--- SumoForkClass.php
+++ SumoForkClass.php
@@ -2,6 +2,8 @@
 namespace SumoCoders\SumoForkClass;
 
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Errbit;
+
 
 class SumoForkClass
 {
@@ -24,5 +26,30 @@ class SumoForkClass
 	public function setContainer(ContainerInterface $container = null)
 	{
 		$this->container = $container;
+	}
+	/**
+	 * Initialize Errbit
+	 */
+	public function initErrbit()
+	{
+		try {
+			$debug = $this->getContainer()->getParameter('fork.debug');
+			$errbitApiKey = $this->getContainer()->getParameter('sumo.errbit_api_key');
+
+			// only activate the error handler when we aren't in debug-mode and an api key is provided
+			if(!$debug && $errbitApiKey != '')
+			{
+				Errbit::instance()->configure(array(
+					'api_key' => $errbitApiKey,
+					'host' => 'errors.sumocoders.be',
+					'secure' => true,
+					'port' => 443,
+				))->start();
+			}
+		}
+		catch (\Symfony\Component\DependencyInjection\Exception\ParameterNotFoundException $e)
+		{
+			// do nothing when a parameter can't be found
+		}
 	}
 }
