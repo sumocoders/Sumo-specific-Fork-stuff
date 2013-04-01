ab389aa4851a4e8dbb849709061f0a7b17dd63ec
Tijs Verkoyen <tijs@verkoyen.eu>
2013-04-01 11:13:33 +0200

    Made it possible to pass the container
    By passing the container we will be able to get parameters from the
    configuration files.

--- SumoForkClass.php
+++ SumoForkClass.php
@@ -1,6 +1,28 @@
 <?php
 namespace SumoCoders\SumoForkClass;
 
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
 class SumoForkClass
 {
+	/**
+	 * @var Symfony\Component\DependencyInjection\ContainerInterface
+	 */
+	private $container;
+
+	/**
+	 * @return ContainerInterface
+	 */
+	public function getContainer()
+	{
+		return $this->container;
+	}
+
+	/**
+	 * @param ContainerInterface[optional] $container
+	 */
+	public function setContainer(ContainerInterface $container = null)
+	{
+		$this->container = $container;
+	}
 }
